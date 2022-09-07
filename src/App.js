35becb3c93b473c302e753e48d1f3719de259a37
Caonabo Mena <cmena7512@gmail.com>
2022-09-07 19:30:50 -0400

    [ADD] base progran structure

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+import './App.css';
+import { TodoCounter } from './Components/TodoCounter/TodoCounter';
+
+function App() {
+  return (
+      <React.Fragment>
+        {/** Counter component **/}
+        <TodoCounter />
+        {/** Search Component **/}
+        {/** Todo List Component **/}
+        {/** Todo Item Component **/}
+        {/** Add Buttom Component **/}
+      </React.Fragment>    
+  );
+}
+
+export default App;
---
+++ src/Components/TodoCounter/TodoCounter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './TodoCounter.css'
+const TodoCounter = () => {
+    return ( 
+        <h2 className='TodoCounter'>Completado 2 de 3 Todo</h2>
+     );
+}
+ 
+export { TodoCounter };
