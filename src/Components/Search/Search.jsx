7d829aaeb2aec5fea713357d6e108e9d52de372d
Caonabo Mena <cmena7512@gmail.com>
2022-09-11 22:02:26 -0400

    [ADD] change searchValueState with user input value

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -4,11 +4,19 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 import "./Search.css"
 
-const SearchBox = () => {
+const SearchBox = ({setSearchValue}) => {
+    const OnChangeHandler = (event) => {
+        setSearchValue(event.target.value)
+    }
     return ( 
         <div className='SearchBox-container'>
             <div className='SearchBox'>
-                <input type='text' className='SearchBox-input' placeholder='Search Task' />
+                <input 
+                onChange={OnChangeHandler}
+                type='text' 
+                className='SearchBox-input' 
+                placeholder='Search Task' 
+                />
                 <button className='SearchBox-button' type='button'>
                     <FontAwesomeIcon className='SearchBox-icon' icon={faMagnifyingGlass} />
                 </button>
